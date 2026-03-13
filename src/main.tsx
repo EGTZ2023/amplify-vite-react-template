5cabc64dcf67de8d17002221f4fb697f7eb69c94
Eric Gutierrez <egtz2023@gmail.com>
2026-03-13 15:02:16 -0400

    safe load amplify outputs

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,15 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 async function start() {
-  const outputs = await import("../amplify_outputs.json");
+  let outputs = {};
 
-  Amplify.configure(outputs.default);
+  try {
+    outputs = (await import("../amplify_outputs.json")).default;
+  } catch {
+    console.warn("amplify_outputs.json not found during build");
+  }
+
+  Amplify.configure(outputs);
 
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
